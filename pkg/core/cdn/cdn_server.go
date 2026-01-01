75e9ce9616e082dd2bb3f2ad01336e070597ce1a
KhoalaS <khoalekappa@gmail.com>
2026-01-02 00:20:11 +0100

    add missing asset

--- pkg/core/cdn/cdn_server.go
+++ pkg/core/cdn/cdn_server.go
@@ -12,14 +12,19 @@ var androidManifest []byte
 //go:embed table_bundlesize.ab.manifest
 var tableBundlesizeManifest []byte
 
+//go:embed table_bundlesize.ab
+var tableBundlesize []byte
+
 func NewCdnServer(port uint16) http.Server {
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /AssetBundles/Android/Android.manifest", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(androidManifest)
 	})
-
 	mux.HandleFunc("GET /AssetBundles/Android/table/table_bundlesize.ab.manifest", func(w http.ResponseWriter, r *http.Request) {
 		w.Write(tableBundlesizeManifest)
+	})
+	mux.HandleFunc("GET /AssetBundles/Android/table/table_bundlesize.ab", func(w http.ResponseWriter, r *http.Request) {
+		w.Write(tableBundlesize)
 	})
 
 	return http.Server{
