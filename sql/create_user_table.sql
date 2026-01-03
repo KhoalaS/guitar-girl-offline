6a922d208211709a0bb653b4a76a1f7015f2e4dd
KhoalaS <khoalekappa@gmail.com>
2026-01-03 20:58:07 +0100

    add table create script

---
+++ sql/create_user_table.sql
@@ -0,0 +1,27 @@
+DROP TABLE user_data;
+CREATE TABLE user_data (
+    uuid TEXT PRIMARY KEY,
+    u_seq INTEGER,
+    u_id TEXT,
+    u_name TEXT,
+    u_nick TEXT,
+    u_cp INTEGER,
+    u_candy REAL,
+    u_like REAL,
+    u_fans INTEGER,
+    u_fans_grade INTEGER,
+    u_selected_costume_id INTEGER,
+    u_selected_music_id INTEGER,
+    u_retain_continuous_tap INTEGER,
+    u_join_type INTEGER,
+    u_last_login TEXT,
+    u_last_communication TEXT,
+    u_save_date TEXT,
+    u_create_time TEXT,
+    u_tutorial_step INTEGER,
+    u_review_popup TEXT,
+    device_uuid TEXT,
+    u_samseck_step INTEGER,
+    u_free_cp INTEGER,
+    u_charge_cp INTEGER
+);
